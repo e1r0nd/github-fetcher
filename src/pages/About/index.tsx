0bc651f558d12469d6571d7b113858246e1f5489
amerezhany <a.merezhany@astoundcommerce.com>
2021-01-20 20:29:24 +0100

    Added pages

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Container from "@material-ui/core/Container";
+
+import { updatePageTitle } "../../components/Navigation/navigationSlice";
+import { localValue } from "../../components/Language/languageSlice";
+
+import { locals } from "../../app/locals";
+import { useStyles } from "./styles";
+import { Typography } from "@material-ui/core";
+
+export function About() {
+  const classes = useStyles();
+  const local = useSelector(localValue);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(updatePageTitle(locals[local].navigation.titleAbout));
+  });
+
+  return (
+    <>
+      <Container maxWidth="sm" className={classes.offset}>
+        <h1 style={{ textAlign: "center" }}>
+          <span
+            aria-label="Diamond"
+            role="img"
+            style={{ fontSize: "80px", display: "block", textAlign: "center" }}
+          >
+            💎
+          </span>
+        </h1>
+        <Typography variant="h6" component="h1">
+          {locals[local].about.title}
+        </Typography>
+        <div className={classes.wrapper}>
+          {locals[local].about.text.map((item: string) => (
+            <Typography
+              variant="body1"
+              component="p"
+              className={classes.paragraph}
+              key={item}
+            >
+              {item}
+            </Typography>
+          ))}
+        </div>
+      </Container>
+    </>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Container } from "@material-ui/core";
+
+import { updatePageTitle } from "../../components/Navigation/navigationSlice";
+import { localValue } from "../../components/Language/languageSlice";
+
+import { locals } from "../../app/locals";
+import { useStyles } from "./styles";
+
+export function Home() {
+  const classes = useStyles();
+  const local = useSelector(localValue);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(updatePageTitle(locals[local].navigation.titleHome));
+  });
+
+  return (
+    <Container maxWidth="sm" className={classes.offset}>
+      <></>
+    </Container>
+  );
+}
