751781682e4783db68a9d6b9c9a800ef0cac1a52
e1r0nd <a.merezhanyi@gmail.com>
2021-01-29 12:24:56 +0100

    fixed Modile width issue

--- src/components/RepoList/styles.ts
+++ src/components/RepoList/styles.ts
@@ -4,6 +4,7 @@ export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      width: '100%',
     },
     learnmore: {
       textTransform: "uppercase",
